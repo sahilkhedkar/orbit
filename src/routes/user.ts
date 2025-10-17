7391a5c190a4d9658612a03820004df3cfaae079
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-10-17 14:51:21 +0530

    implimented zod validaton

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,6 @@
 import { Router } from "express"
+import  jwt  from "jsonwebtoken"
+import z from 'zod'
 
 export const UserRouter = Router()
 
@@ -10,10 +12,26 @@ enum ResponseStatus {
     Error_in_input = 413
 }
 
+const userProfileSchema = z.object({
+  name: z.string().min(1, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  age: z.number().min(18, { message: "You must be at least 18 years old" }).optional(),
+});
+
+type Profile = z.infer<typeof userProfileSchema>
+
+
 UserRouter.post("/signup" , (req ,res) => {
-    res.status(ResponseStatus.Success).json({
-        msg: "You are Signed Up"
-    })
+    const parsedDatawithSuccess = userProfileSchema.safeParse(req.body)
+
+    if(!parsedDatawithSuccess.success) {
+        return res.status(ResponseStatus.Error_in_input).json({
+            msg: "Incorrect Format",
+            error: parsedDatawithSuccess.error
+        })
+    }
+    
+    
 })
 
 UserRouter.post("/signin" , (req ,res) => {
