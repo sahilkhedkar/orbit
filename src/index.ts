b6c455692df0f2c7815149fc3110891e9aa98060
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-10-25 19:25:23 +0530

    initialized skeleton routes

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { userRouter } from './routes/userRouter.js';
+
+dotenv.config({
+    path: ".env"
+})
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.use("/api/v1/user" , userRouter);
---
+++ src/routes/userRouter.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+
+export const userRouter = Router();
+
+userRouter.post("/signup" , async (req,res) => {
+
+})
+
+userRouter.post("/signin" , async (req,res) => {
+    
+})
+
+userRouter.post("/content" , async (req,res) => {
+    
+})
+
+userRouter.get("/content" , async (req,res) => {
+    
+})
+
+userRouter.delete("/content" , async (req,res) => {
+    
+})
+
+userRouter.post("/orbit/share" , async (req,res) => {
+    
+})
+
+userRouter.post("/orbit/:shareLink" , async (req,res) => {
+    
+})
